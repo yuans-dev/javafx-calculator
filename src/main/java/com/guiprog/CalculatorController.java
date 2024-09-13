d2258edd1a707cbf69cf9f1b650687f6ebc437da
Yuan <u1benedict425@gmail.com>
2024-09-13 20:32:56 +0800

    import sample project

---
+++ src/main/java/com/guiprog/CalculatorController.java
@@ -0,0 +1,157 @@
+package com.guiprog;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Group Name: <Group Name>
+ * Group Members: <Group Members>
+ * Course/Section: <Course/Section>
+ */
+
+public class CalculatorController {
+
+    @FXML
+    Label calcDisplay;
+    @FXML
+    Button calcPad1;
+    @FXML
+    Button calcPad2;
+    @FXML
+    Button calcPad3;
+    @FXML
+    Button calcPad4;
+    @FXML
+    Button calcPad5;
+    @FXML
+    Button calcPad6;
+    @FXML
+    Button calcPad7;
+    @FXML
+    Button calcPad8;
+    @FXML
+    Button calcPad9;
+    @FXML
+    Button calcPad0;
+    @FXML
+    Button calcPadAdd;
+    @FXML
+    Button calcPadMinus;
+    @FXML
+    Button calcPadMultiply;
+    @FXML
+    Button calcPadDivide;
+    @FXML
+    Button calcPadDot;
+    @FXML
+    Button calcPadEquals;
+    @FXML
+    Button calcPadClear;
+    @FXML
+    Button calcPadNegative;
+    @FXML
+    Button calcPadDelete;
+    @FXML
+    Button calcPadCE;
+
+    @FXML
+    private void handleCalcPad1() {
+        calcDisplay.setText("handleCalcPad1");
+    }
+
+    @FXML
+    private void handleCalcPad2() {
+        calcDisplay.setText("handleCalcPad2");
+    }
+
+    @FXML
+    private void handleCalcPad3() {
+        calcDisplay.setText("handleCalcPad3");
+    }
+
+    @FXML
+    private void handleCalcPad4() {
+        calcDisplay.setText("handleCalcPad4");
+    }
+
+    @FXML
+    private void handleCalcPad5() {
+        calcDisplay.setText("handleCalcPad5");
+    }
+
+    @FXML
+    private void handleCalcPad6() {
+        calcDisplay.setText("handleCalcPad6");
+    }
+
+    @FXML
+    private void handleCalcPad7() {
+        calcDisplay.setText("handleCalcPad7");
+    }
+
+    @FXML
+    private void handleCalcPad8() {
+        calcDisplay.setText("handleCalcPad8");
+    }
+
+    @FXML
+    private void handleCalcPad9() {
+        calcDisplay.setText("handleCalcPad9");
+    }
+
+    @FXML
+    private void handleCalcPad0() {
+        calcDisplay.setText("handleCalcPad0");
+    }
+
+    @FXML
+    private void handleCalcPadAdd() {
+        calcDisplay.setText("handleCalcPadAdd");
+    }
+
+    @FXML
+    private void handleCalcPadMinus() {
+        calcDisplay.setText("handleCalcPadMinus");
+    }
+
+    @FXML
+    private void handleCalcPadMultiply() {
+        calcDisplay.setText("handleCalcPadMultiply");
+    }
+
+    @FXML
+    private void handleCalcPadDivide() {
+        calcDisplay.setText("handleCalcPadDivide");
+    }
+
+    @FXML
+    private void handleCalcPadDot() {
+        calcDisplay.setText("handleCalcPadDot");
+    }
+
+    @FXML
+    private void handleCalcPadEquals() {
+        calcDisplay.setText("handleCalcPadEquals");
+    }
+
+    @FXML
+    private void handleCalcPadClear() {
+        calcDisplay.setText("handleCalcPadClear");
+    }
+
+    @FXML
+    private void handleCalcPadNegative() {
+        calcDisplay.setText("handleCalcPadNegative");
+    }
+
+    @FXML
+    private void handleCalcPadDelete() {
+        calcDisplay.setText("handleCalcPadDelete");
+    }
+
+    @FXML
+    private void handleCalcPadCE() {
+        calcDisplay.setText("handleCalcPadCE");
+    }
+}
